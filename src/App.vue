eb69cda1bb16e1c386d6dfbff8d00d8a31ac56ba
Josip Ivancic <ivancic.josip@nsoft.com>
2021-11-07 09:20:00 +0100

    Add sass setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div class="example">
+		<HelloWorld />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from '@/components/HelloWorld';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+	components: {
+		HelloWorld,
+	},
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@use "./styles/main.scss";
+.example {
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 }
 </style>
